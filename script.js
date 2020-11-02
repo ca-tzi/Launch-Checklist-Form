bbffa465a9dcf2318d689062f398484954438f5a
ca-tzi <68138133+ca-tzi@users.noreply.github.com>
2020-11-01 19:08:50 -0500

    updated js

--- script.js
+++ script.js
@@ -14,74 +14,102 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event){
+
+
+      // initializing variables. first four take the input of the four forms and put them in a variable
+      // the next seven initialize id's and classes in the html as variables
+
+
       let pilotInput = document.querySelector("input[name=pilotName]");
       let copilotInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-      let copilotStatus = document.querySelector("flightStatus");
-      let pilotStatus = document.querySelector("pilotStatus");
-      let faulty = document.querySelector("faultyItems");
-      let statusText = document.querySelector("launchStatus");
-      let fuelStatus = document.querySelector('fuelStatus');
-      let cargoStatus = document.querySelector('cargoStatus');
-      let missionTarget = document.querySelector('missionTarget');
-
-      // <li id="pilotStatus">Pilot Ready</li>
-      //               <li id="copilotStatus">Co-pilot Ready</li>
-
-      //fuelLevelInput = Number(fuelLevelInput.value);
-      // let inputTest = Number(fuelLevelInput.value);
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
-      //cargoMassInput = Number(cargoMassInput.value);
-      // let cargoMassTest = Number(cargoMassInput.value);
+      let copilotStatus = document.querySelector("copilotStatus"); //should this be flightStatus or copilotStatus??? -- come back to this if error arises
+      let pilotStatus = document.querySelector("pilotStatus");
+      let faultyItems = document.querySelector("faultyItems");
+      let statusText = document.querySelector("launchStatus");
+      let fuelStatus = document.querySelector("fuelStatus");
+      let cargoStatus = document.querySelector("cargoStatus");
+      
+
+      // if a field is empty, throw an error and refuse to load the input
+
 
       if (pilotInput.value === "" || copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === ""){
          alert("All fields are required.");
          event.preventDefault();
+
+
+      // if the wrong type of input is entered in a field, throw an error and refuse to load the input
+
+
       }else if(isNaN(pilotInput.value) === false|| isNaN(copilotInput.value) === false || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value) ){
          alert("Invalid Input... :(");
          event.preventDefault();
-      }else{
-         
-         if ( fuelLevelInput.value < 10000){
-            fuelStatus.addEventListener('submit', function(event){
-               faulty.style.visibility = 'visible';
-            statusText.style.color = 'red';
-            statusText.innerHTML = 'Shuttle not ready for launch';
-            fuelStatus.innerHTML = 'Fuel level too low for launch.';
-            })
-            
-         if(cargoMassInput.value > 10000){
-            cargoStatus.addEventListener('submit', function(event){
-               cargoStatus.innerHTML = 'Fuel level too low for launch.';
-            })
-         }
-         copilotInput.addEventListener("submit", function(event){
-            copilotStatus.innerHTML = `${copilotStatus.value} is ready for launch!`
-         })
-         pilotInput.addEventListener("submit", function(event){
-            pilotStatus.innerHTML = `${pilotStatus.value} is ready for launch!`
-         })
-         }else{
-         statusText.style.color = 'green';
-         statusText.innerHTML = 'Shuttle is ready for launch!'
-         let json = []
 
-            fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-         response.json().then(function(json){
-            missionTarget.innerHTML = 
-            `<h2>Mission Destination</h2>
-            <ol>
-               <li>Name: ${json[0].name}</li>
-               <li>Diameter: ${json[0].diameter}</li>
-               <li>Star: ${json[0].star}</li>
-               <li>Distance from Earth: ${json[0].distance}</li>
-               <li>Number of Moons: ${json[0].moons}</li>
-            </ol>
-            <img src="${json[0].image}">`;
+
+      //if all fields are filled with passable input, proceed
+
+
+      }else{
+
+
+            //update copilot and pilot status regardless of other inputs
+
+
+            copilotStatus.innerHTML = `${copilotStatus.value} is ready for launch!`
+            pilotStatus.innerHTML = `${pilotStatus.value} is ready for launch!`
+
+
+            // if the fuel level is too low, or the cargo mass is too high, the box at the bottom of the screen changes its appearance, 
+            // listing the un-passable values and letting the user know shuttle is not ready for launch
+
+
+            if ( fuelLevelInput.value < 10000 || cargoMassInput.value > 10000){
+               faultyItems.style.visibility = 'visible';
+               statusText.style.color = 'red';
+               statusText.innerHTML = 'Shuttle not ready for launch';
+
+
+               //checks which attributes triggered faultyItems, then changes their status
+
+
+               if (cargoMassInput.value > 10000){
+                  cargoStatus.innerHTML = 'Cargo mass too high for launch.';
+               }
+               if (fuelLevelInput.value < 10000){
+                  fuelStatus.innerHTML = 'Fuel level too low for launch';
+               }
+
+            //if all is good, shuttle is ready for launch and location is designated
+
+            }else{
+               
+               //changes status text color to green, and what the text actually says
+
+
+               statusText.style.color = "green";
+               statusText.innerHTML = "Shuttle is ready for launch!"
+
+               //fetch json from link, then post in missionTarget div
+
+                  fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+                  response.json().then(function(json){
+                  let missionTarget = document.querySelector("missionTarget");
+                  missionTarget.innerHTML = 
+                  `<h2>Mission Destination</h2>
+                  <ol>
+                     <li>Name: ${json[0].name}</li>
+                     <li>Diameter: ${json[0].diameter}</li>
+                     <li>Star: ${json[0].star}</li>
+                     <li>Distance from Earth: ${json[0].distance}</li>
+                     <li>Number of Moons: ${json[0].moons}</li>
+                  </ol>
+                  <img src='${json[0].image}'>`;
+               });
             });
-       });
-         //put your pictures in here, nested if/else statement
+         }
       }
-   };
+   });
 });
-});
+
